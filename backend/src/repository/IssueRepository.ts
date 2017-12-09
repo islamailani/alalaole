b5cce556c73ffd24c4822dfc3e0c41e9cbd288cd
Darius Costolas <dariuscostolas@yahoo.com>
2017-12-09 03:47:32 +0200

    Implement issue finding within a circle and out of one

--- backend/src/repository/IssueRepository.ts
+++ backend/src/repository/IssueRepository.ts
@@ -1,5 +1,5 @@
 import { injectable } from 'inversify';
-import { createConnection, Repository, getManager } from 'typeorm';
+import { createConnection, getManager, Repository } from 'typeorm';
 
 import { Config } from '../config/Config';
 
@@ -9,6 +9,8 @@ import { Photo } from '../models/Photo';
 export interface IssueRepository {
     create(issue: Issue): Promise<Issue>;
     createPhoto(photo: Photo): Promise<Photo>;
+    getAll(from: number): Promise<Issue[]>;
+    getAllInCircle(from: number, centerX: number, centerY: number, radius: number);
 }
 
 @injectable()
@@ -27,6 +29,27 @@ export class IssueRepositoryImplDb implements IssueRepository {
 
     public async createPhoto(photo: Photo): Promise<Photo> {
         return await this.photoRepository.save(photo);
+    }
+
+    public async getAll(from: number): Promise<Issue[]> {
+        return await this.issueRepository
+            .createQueryBuilder('i')
+            .leftJoinAndSelect('i.photos', 'photo')
+            .leftJoinAndSelect('i.location', 'location')
+            .skip(from)
+            .take(30)
+            .getMany();
+    }
+
+    public async getAllInCircle(from: number, centerX: number, centerY: number, radius: number) {
+        return await this.issueRepository
+            .createQueryBuilder('i')
+            .leftJoinAndSelect('i.location', 'location')
+            .leftJoinAndSelect('i.photos', 'photo')
+            .where('((location.latitude - :centerX)^2 + (location.longitude - :centerY)^2) < :radius ^ 2', { centerX, centerY, radius })
+            .skip(from)
+            .take(30)
+            .getMany();
     }
 
 }
