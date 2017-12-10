7cfb0ea16420d555aedd240d63e25d6c7860da5f
Darius Costolas <dariuscostolas@yahoo.com>
2017-12-10 07:54:10 +0200

    Fix CORS

--- backend/src/middlewares/CORSMiddleware.ts
+++ backend/src/middlewares/CORSMiddleware.ts
@@ -6,7 +6,7 @@ import container from '../inversify.config';
 export default async function CORS(req: express.Request, res: express.Response, next: express.NextFunction) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
-    res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
     /* tslint:disable */ 
     res.setHeader('Access-Control-Allow-Headers',
         `Access-Control-Allow-Headers, Origin,Accept, x-api-token, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers`);
